57f56180c54e67fdef7b4cf37ef8de044540d79d
FilippoToffoletto <filippotoffoletto@hotmail.it>
2023-02-15 18:17:29 +0100

    [add]media query breakepoint

---
+++ resources/views/admin/foods/index.blade.php
@@ -0,0 +1,167 @@
+@extends('layouts.admin')
+
+@section('title')
+
+    | Index
+
+@endsection
+
+@section('content')
+
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="">
+                <div class="card-header">
+                    <h2 class="m-0 mt-3">Menù</h2>
+                </div>
+
+                <main>
+                    <div class="container-xl my-4 p-3">
+
+                        <!-- 1 card -->
+                        <div class="row row-cols-1 row-cols-xs-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3">
+                            <div class="col col-md-6 col-lg-4 col-xl-3">
+                                <div class="card">
+                                    <img src="https://www.mcdonalds.it/sites/default/files/styles/product_isolated_preview/public/products/big-mac-isolated.png" class="card-img-top" alt="hamburger">
+                                    <div class="card-body">
+                                        <div class="clearfix mb-3">
+                                            <button type="button" class="btn btn-info mt-1">
+                                                <a class="text-white text-decoration-none" href="#">Visualizza</a>
+                                            </button>
+                                            <span class="float-end">
+                                                4.00&euro;
+                                            </span>
+                                        </div>
+                                        <h5 class="card-title">
+                                            Hamburger
+                                        </h5>
+                                        <div class="text-left my-4">
+                                            <h6>Ingredienti:</h6>
+                                            <p class="text-capitalize">
+                                                pane, carne bovina, formaggio, salsa big mac, insalata, cipolla, cetriolo.
+                                            </p>
+                                        </div>
+                                        <div class="">
+                                            <div class="d-flex justify-content-between">
+                                                <button type="button" class="btn btn-warning mt-1">
+                                                    <a class="text-white text-decoration-none" href="#">Modifica</a>
+                                                </button>
+                                                <button type="button" class="btn btn-danger">
+                                                    Elimina</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col col-md-6 col-lg-4 col-xl-3">
+                                <div class="card">
+                                    <img src="https://www.mcdonalds.it/sites/default/files/styles/product_isolated_preview/public/products/big-mac-isolated.png" class="card-img-top" alt="hamburger">
+                                    <div class="card-body">
+                                        <div class="clearfix mb-3">
+                                            <button type="button" class="btn btn-info mt-1">
+                                                <a class="text-white text-decoration-none" href="#">Visualizza</a>
+                                            </button>
+                                            <span class="float-end">
+                                                4.00&euro;
+                                            </span>
+                                        </div>
+                                        <h5 class="card-title">
+                                            Hamburger
+                                        </h5>
+                                        <div class="text-left my-4">
+                                            <h6>Ingredienti:</h6>
+                                            <p class="text-capitalize">
+                                                pane, carne bovina, formaggio, salsa big mac, insalata, cipolla, cetriolo.
+                                            </p>
+                                        </div>
+                                        <div class="">
+                                            <div class="d-flex justify-content-between">
+                                                <button type="button" class="btn btn-warning mt-1">
+                                                    <a class="text-white text-decoration-none" href="#">Modifica</a>
+                                                </button>
+                                                <button type="button" class="btn btn-danger">
+                                                    Elimina</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col col-md-6 col-lg-4 col-xl-3">
+                                <div class="card">
+                                    <img src="https://www.mcdonalds.it/sites/default/files/styles/product_isolated_preview/public/products/big-mac-isolated.png" class="card-img-top" alt="hamburger">
+                                    <div class="card-body">
+                                        <div class="clearfix mb-3">
+                                            <button type="button" class="btn btn-info mt-1">
+                                                <a class="text-white text-decoration-none" href="#">Visualizza</a>
+                                            </button>
+                                            <span class="float-end">
+                                                4.00&euro;
+                                            </span>
+                                        </div>
+                                        <h5 class="card-title">
+                                            Hamburger
+                                        </h5>
+                                        <div class="text-left my-4">
+                                            <h6>Ingredienti:</h6>
+                                            <p class="text-capitalize">
+                                                pane, carne bovina, formaggio, salsa big mac, insalata, cipolla, cetriolo.
+                                            </p>
+                                        </div>
+                                        <div class="">
+                                            <div class="d-flex justify-content-between">
+                                                <button type="button" class="btn btn-warning mt-1">
+                                                    <a class="text-white text-decoration-none" href="#">Modifica</a>
+                                                </button>
+                                                <button type="button" class="btn btn-danger">
+                                                    Elimina</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col col-md-6 col-lg-4 col-xl-3">
+                                <div class="card">
+                                    <img src="https://www.mcdonalds.it/sites/default/files/styles/product_isolated_preview/public/products/big-mac-isolated.png" class="card-img-top" alt="hamburger">
+                                    <div class="card-body">
+                                        <div class="clearfix mb-3">
+                                            <button type="button" class="btn btn-info mt-1">
+                                                <a class="text-white text-decoration-none" href="#">Visualizza</a>
+                                            </button>
+                                            <span class="float-end">
+                                                4.00&euro;
+                                            </span>
+                                        </div>
+                                        <h5 class="card-title">
+                                            Hamburger
+                                        </h5>
+                                        <div class="text-left my-4">
+                                            <h6>Ingredienti:</h6>
+                                            <p class="text-capitalize">
+                                                pane, carne bovina, formaggio, salsa big mac, insalata, cipolla, cetriolo.
+                                            </p>
+                                        </div>
+                                        <div class="">
+                                            <div class="d-flex justify-content-between">
+                                                <button type="button" class="btn btn-warning mt-1">
+                                                    <a class="text-white text-decoration-none" href="#">Modifica</a>
+                                                </button>
+                                                <button type="button" class="btn btn-danger">
+                                                    Elimina</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </main>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
