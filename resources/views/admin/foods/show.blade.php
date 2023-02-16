f6a20e0a364bdfd8488120bf93088579f19055bc
FilippoToffoletto <filippotoffoletto@hotmail.it>
2023-02-16 11:09:54 +0100

    [add]show pc

--- resources/views/admin/foods/show.blade.php
+++ resources/views/admin/foods/show.blade.php
@@ -11,7 +11,7 @@
         <div class="">
 
             <div class="card">
-                <div class="row row-cols-1 row-cols-xs-1 d-flex justify-content-between">
+                <div class="row row-cols-1 row-cols-xs-1 row-cols-xl-2 d-flex justify-content-between">
 
                     <!--    NOME PIATTO/PREZZO -->
                     <div class="col p-3 px-5">
@@ -23,10 +23,9 @@
                                 Hamburger
                             </h1>
                         </div>
-                        <div class="text-left my-1 d-flex justify-content-between">
+                        <div class="text-left my-3 d-flex justify-content-between">
                             <h3>Ingredienti:</h3>
                         </div>
-
                         <!--    INGREDIENTI -->
                         <div>
                             <p class="text-capitalize w-75 fs-5">
@@ -37,22 +36,22 @@
 
                         <!--    BOTTONI FONTAWESOME -->
                         <div class="">
-                            <div class="d-flex justify-content-between">
-                                <button type="button" class="btn btn-success mt-1">
+                            <div class="row">
+                                <button type="button" class="col-2 mx-2 btn btn-success mt-1">
                                     <a class="text-white text-decoration-none" href="{{route('admin.foods.index')}}">
                                         TORNA AL MEN&Uacute;
                                     </a>
                                 </button>
-                                <button type="button" class="btn btn-warning mt-1">
+                                <button type="button" class="col mx-2 btn btn-warning mt-1">
                                     <a class="text-white text-decoration-none" href="#">
                                         MODIFICA
                                     </a>
                                 </button>
-                                <button type="button" class="btn btn-danger mt-1">
+                                <button type="button" class="col mx-2 btn btn-danger mt-1">
                                     <a href="" class="text-white text-decoration-none">ELIMINA</a>
                                 </button>
                                 <!--    PULSANTE ON/OFF -->
-                                <div class="form-check form-switch">
+                                <div class=" col mx-2 form-check form-switch">
                                     <label class="form-check-label" for="flexSwitchCheckChecked"></label>
                                     <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked>
                                     <span>ON/OFF</span>
