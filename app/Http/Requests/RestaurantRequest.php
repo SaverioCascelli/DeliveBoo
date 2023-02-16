07014d276b9299a74b67f3b67a391a78a203b303
LupoArticoblu <wolf94-@libero.it>
2023-02-16 15:30:00 +0100

    creazione rules e messaggi di errore

--- app/Http/Requests/RestaurantRequest.php
+++ app/Http/Requests/RestaurantRequest.php
@@ -24,7 +24,29 @@ class RestaurantRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            
+            'name' => 'required|max:200|min:2',
+            'VAT' => 'required|max:11|min:11',
+            'address' => 'required|max:150|min:8',
+            'img_url' => 'nullable|image|max:64000'
+        ];
+    }
+    public function message()
+    {
+        return [
+            
+            'name.required' => 'Inserisci un nome valido',
+            'name.max' => 'Inserisci un nome valido di massimo 200 caratteri',
+            'name.min' => 'Inserisci un nome valido di minimo 2 caratteri',
+            'VAT.required' => 'Inserire la P.IVA',
+            'VAT.max' => 'Inserire la P.IVA (11 caratteri)',
+            'VAT.min' => 'Inserire la P.IVA (11 caratteri)',
+            'address.required' => 'Inserisci l\'ubicazione del tuo ristorante',
+            'address.max' => 'Inserisci un massimo di 150 caratteri',
+            'address.min' => 'Inserisci un minimo di 8 caratteri',
+            'img_url.image' => 'Inserisci un file immagine',
+            'img_url.max' => 'Immagine troppo grande'
+
         ];
     }
 }
