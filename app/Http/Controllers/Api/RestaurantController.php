f57838742b8bd38e80b8c0e29e5be10504e4b252
MarBozzz <mbozzuto@libero.it>
2023-02-18 17:32:54 +0100

    [added] API search by type

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -3,13 +3,27 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Food;
 use App\Models\Restaurant;
+use App\Models\Type;
 use Illuminate\Http\Request;
 
 class RestaurantController extends Controller
 {
     public function index(){
-        $restaurants = Restaurant::all();
-            return response()->json(compact('restaurants'));
-        }
+        $types = Type::all();
+        $foods = Food::all();
+        $restaurants = Restaurant::with(['types','foods','user'])->get();
+        //dd($restaurants);
+            return response()->json(compact('restaurants','types','foods'));
+    }
+
+
+    public function getByType(){
+        $typeIds = [];
+        $restaurants = Restaurant::whereHas('types', function ($query) use ($typeIds) {
+        $query->whereIn ('id', $typeIds);
+        }, '>', count($typeIds)-1)->get();
+        return response()->json(compact('restaurants'));
+    }
 }
