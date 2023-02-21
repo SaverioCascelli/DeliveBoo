9e97d6ad45cd98cd3a996b93cc938b2e9707c536
SaverioCascelli <saverio.cascelli@gmail.com>
2023-02-22 00:32:28 +0100

    [add] cart functionality

--- resources/js/data/function.js
+++ resources/js/data/function.js
@@ -14,4 +14,111 @@ const getImagePath = (imageName) => {
 return new URL(`../img/${imageName}`, import.meta.url).href
 }
 
-export {getImagePath};
+
+function addFood(foodId){
+
+    let storageRestaurant = localStorage.getItem('restaurantSlug');
+    //console.log(localStorage);
+    //se in localStorage esiste restaurantName && in store orderRestaurantSlug è uguale a ''
+    if (!(storageRestaurant ) && this.store.orderRestaurantSlug == '') {
+        this.store.orderRestaurantSlug= this.store.currentRestaurant.slug;
+        window.localStorage.setItem('restaurantSlug', this.store.orderRestaurantSlug);
+
+    } else if (this.store.currentRestaurant.slug != storageRestaurant) {
+        console.log('ristorante diverso');
+
+        //todo logica ristorante diverso
+        return;
+    }
+
+    this.getLocalStorage();
+
+    this.store.orderItems.push(foodId);
+
+    this.setLocalStorage();
+
+}
+
+//rimuove un id di food da orderItems in store e order in localstorage
+function removeFood(foodId) {
+    this.getLocalStorage();
+
+    //cerca il primo elemento con l'id da ricercare e lo rimuove
+    var index = this.store.orderItems.findIndex(element => element == foodId);
+    if (index != -1) {
+        this.store.orderItems.splice(index, 1);
+
+    }
+
+    this.setLocalStorage();
+
+    //se l'array di foods da comprare è vuoto, svuota localtorage con il nome del ristorante così da poterne mettere un altro
+    if (this.store.orderItems.length == 0) {
+        this.clearOrder();
+        console.log(localStorage);
+    }
+}
+
+//mette l'ordine in localstorage e in store
+function getLocalStorage() {
+    //console.log('getLocalStorage');
+    if (window.localStorage.order) {
+        //console.log('prendo order da localstorage');
+
+        //trasforma il json di localstorage in un array di id(food) e lo salva in store
+        let orderString = localStorage.getItem('order');
+        this.store.orderItems= JSON.parse(orderString);
+        this.store.orderRestaurantSlug = localStorage.getItem('restaurantSlug')
+        // console.log('localstorage');
+        // console.log(localStorage);
+        // console.log('order');
+        // console.log(this.store.orderRestaurantSlug);
+        // console.log(this.store.orderItems);
+    }
+}
+
+//mette l'ordine da store in localstorage
+function setLocalStorage() {
+    //console.log('setLocalStorage');
+    //trasforma l'array in store in una stringa json e la salva in localstorage
+    let orderJson = JSON.stringify(this.store.orderItems);
+    window.localStorage.setItem('order', orderJson);
+}
+
+//pulisce TUTTO il localstorage
+function clearOrder() {
+    this.store.orderRestaurantSlug = '',
+    this.store.orderItems = [];
+    //console.log(localStorage);
+    localStorage.clear();
+    //console.log('clear storage');
+    //console.log(localStorage);
+}
+
+//prende da store l'array di id  orderItems e ritorna la quantità dell'id cercato
+function getQuantity(foodId) {
+    //console.log('getQuantity');
+    var quantity = this.store.orderItems.filter(element => element == foodId);
+    return quantity.length;
+}
+
+function apiOrderRestaurant(){
+
+        this.axios.get('http://127.0.0.1:8000/api/restaurants/show', {
+            params: {
+
+                name: this.store.orderRestaurantSlug
+            }
+        })
+            .then(result => {
+                store.currentRestaurant = result.data.restaurant;
+        })
+
+}
+
+function getFood(id){
+    let foods = this.store.currentRestaurant;
+    //console.log(foods);
+}
+
+export {getImagePath,getQuantity,clearOrder,setLocalStorage,getLocalStorage,removeFood,addFood,apiOrderRestaurant,getFood};
--- resources/js/pages/RestaurantShow.vue
+++ resources/js/pages/RestaurantShow.vue
@@ -5,24 +5,71 @@ permette di visualizzare il menù di un particolare ristoratore.
 È possibile scegliere i cibi desiderati e relativa quantità per inserirli nel carrello. Il carrello si popola con i cibi selezionati e le relative quantità. -->
 
 <script>
+import axios from 'axios';
 
-    export default {
+import { setLocalStorage, getLocalStorage, getQuantity, removeFood, addFood, clearOrder } from '../data/function';
+import { store } from '../data/store';
 
-        name:'RestaurantShow'
 
+export default {
+
+    name: 'RestaurantShow',
+    data() {
+        return {
+            restaurant: {},
+            foods: [],
+            store,
+
+
+            //****funzioni richiamate da function.js***
+            setLocalStorage,
+            getLocalStorage,
+            getQuantity,
+            removeFood,
+            addFood,
+            clearOrder,
+            //***fine funzioni chiamate da function.js */
+
+        }
+    },
+    methods: {
+        apiCall() {
+            axios.get('http://127.0.0.1:8000/api/restaurants/show', {
+                params: {
+
+                    name: this.$route.params.slug
+                }
+            })
+                .then(result => {
+                    store.currentRestaurant = result.data.restaurant;
+                    this.restaurant = store.currentRestaurant;
+                    this.foods = store.currentRestaurant.foods;
+                    console.log(store.currentRestaurant.foods);
+                })
+        }
+    },
+    mounted() {
+        this.apiCall();
+        this.getLocalStorage();
     }
+}
+
 
 </script>
 
 
 <template>
-
     <h1>DETTAGLIO RISTORANTE</h1>
+    <div v-for="food in foods">
 
+        <div class="food" id="{{food.id}}" name="{{food.name}}">
+            <span>{{ food.name }}</span>
+            <button @click="removeFood(food.id)">remove</button>
+            <span>quantity : {{ getQuantity(food.id) }}</span>
+            <button @click="addFood(food.id)">add</button>
+        </div>
+    </div>
 </template>
 
 
-<style lang="scss" scoped>
-
-
-</style>
+<style lang="scss" scoped></style>
