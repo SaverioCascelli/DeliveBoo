aa8c2821f8080aa664ee3382b4c3a0fa65c97d41
RiccardoCivardi <riccardo.civardi@icloud.com>
2023-02-21 21:14:50 +0100

    [fix] home vue

--- resources/js/components/RestaurantItem.vue
+++ resources/js/components/RestaurantItem.vue
@@ -4,7 +4,13 @@
 
     export default {
 
-        name:'RestaurantItem'
+        name:'RestaurantItem',
+
+        props: {
+
+            restaurant: Object
+
+        }
 
     }
 
@@ -13,12 +19,38 @@
 
 <template>
 
-    <h1>CARD RISTORANTE</h1>
+    <div class="col mb-2 mb-md-3">
+        <div class="card restaurant overflow-hidden h-100 bg-success">
+            <div class="restaurant-image">
+                <img :src="restaurant.img_url" :alt="restaurant.name">
+            </div>
+            <p class="mb-0 text-center pt-1">{{ restaurant.name }}</p>
+            <small class="address text-center pb-1">{{ restaurant.address }}</small>
+        </div>
+    </div>
 
 </template>
 
 
 <style lang="scss" scoped>
+
+    .restaurant {
+        cursor: pointer;
+        .restaurant-image{
+            width: 100%;
+            height: 200px;
+            overflow: hidden;
+            img{
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+            }
+        }
+        .address {
+            font-size: 0.7rem
+        }
+    }
+
 
 
 </style>
