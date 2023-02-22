627b7fda7fba4eb0bbfbe6b95846e7ee65a1237a
SaverioCascelli <saverio.cascelli@gmail.com>
2023-02-22 12:50:11 +0100

    [fix] orderItems array bugs

--- resources/js/data/function.js
+++ resources/js/data/function.js
@@ -15,8 +15,10 @@ return new URL(`../img/${imageName}`, import.meta.url).href
 }
 
 
+//aggiunge un entità allo store.orderItems creando un array che contiene oggetti es [{id:2, quantity:3},{id:7, quantity:1}] gli id devono essere tutti dei foods dello stesso restaurant
 function addFood(foodId){
 
+    //**controllo univocità del ristorante */
     let storageRestaurant = localStorage.getItem('restaurantSlug');
     //console.log(localStorage);
     //se in localStorage esiste restaurantName && in store orderRestaurantSlug è uguale a ''
@@ -25,88 +27,116 @@ function addFood(foodId){
         window.localStorage.setItem('restaurantSlug', this.store.orderRestaurantSlug);
 
     } else if (this.store.currentRestaurant.slug != storageRestaurant) {
-        console.log('ristorante diverso');
+         console.log('ristorante diverso');
+
+        //todo logica ristorante diverso
+        return;
+    }
+    //***fine controllo univocità del ristorante */
+
+    this.getLocalStorage();
+
+    //crea l'oggetto es {id:2, quantity:3} e lo pusha entro orderItems se l'id è già presente aumenta la quantity
+    let orderItems = this.store.orderItems;
+    let index = orderItems.findIndex(element => element['id'] == foodId);
+    if(index == -1){
+        let obj = {id:foodId, quantity: 1};
+        this.store.orderItems.push(obj);
+    }else{
+        orderItems[index].quantity++;
+        this.store.orderItems = orderItems;
+    }
+
+    this.setLocalStorage();
+
+}
+
+//rimuove una quantity di food da orderItems in store, se diventa 0, rimuove l'oggetto con l' id dall'array
+function removeFood(foodId) {
+
+     /** controllo univocità del ristorante */
+    let storageRestaurant = localStorage.getItem('restaurantSlug');
+    //console.log(localStorage);
+    //se in localStorage esiste restaurantName && in store orderRestaurantSlug è uguale a ''
+    if (!(storageRestaurant ) && this.store.orderRestaurantSlug == '') {
+        this.store.orderRestaurantSlug= this.store.currentRestaurant.slug;
+        window.localStorage.setItem('restaurantSlug', this.store.orderRestaurantSlug);
+
+    } else if (this.store.currentRestaurant.slug != storageRestaurant) {
+         console.log('ristorante diverso');
 
         //todo logica ristorante diverso
         return;
     }
 
+     /**fine controllo univocità del ristorante */
+
     this.getLocalStorage();
 
-    this.store.orderItems.push(foodId);
-
-    this.setLocalStorage();
-
-}
-
-//rimuove un id di food da orderItems in store e order in localstorage
-function removeFood(foodId) {
-    this.getLocalStorage();
-
-    //cerca il primo elemento con l'id da ricercare e lo rimuove
-    var index = this.store.orderItems.findIndex(element => element == foodId);
-    if (index != -1) {
-        this.store.orderItems.splice(index, 1);
-
+    //diminuisce la quantity del food di riferimento, se arriva a 0 rimuove l'id da orderItems
+    let orderItems = this.store.orderItems;
+    let index = orderItems.findIndex(element => element['id'] == foodId);
+    if(index != -1){
+        if(orderItems[index].quantity > 1){
+            orderItems[index].quantity--;
+            this.store.orderItems = orderItems;
+        }else{
+            //console.log(index);
+            orderItems.splice(index,1);
+            this.store.orderItems = orderItems;
+        }
     }
 
     this.setLocalStorage();
-
+    //console.log('remove');
+    // console.log(this.store.orderItems );
     //se l'array di foods da comprare è vuoto, svuota localtorage con il nome del ristorante così da poterne mettere un altro
     if (this.store.orderItems.length == 0) {
         this.clearOrder();
-        console.log(localStorage);
+        // console.log('clearorder');
     }
 }
 
-//mette l'ordine in localstorage e in store
+//prende il localStorage e lo mette in store, in localstorage troviamo il ristorante dell'ordine(completo di foods), lo slug del ristorante dell'ordine e gli item messi nel carrello
 function getLocalStorage() {
     //console.log('getLocalStorage');
     if (window.localStorage.order) {
         //console.log('prendo order da localstorage');
 
-        //trasforma il json di localstorage in un array di id(food) e lo salva in store
+        //prende currentRestaurant e orderItems da localstorage in formato JSON e li trasforma in array e li salva in store
         let orderString = localStorage.getItem('order');
         this.store.orderItems= JSON.parse(orderString);
-        this.store.orderRestaurantSlug = localStorage.getItem('restaurantSlug')
-
-        let order = [];
-
-       this.store.orderItems.forEach(foodId => {
-        let index = order.findIndex(element => element['id']== foodId);
-        if (index == -1){
-            let obj = {id:foodId, quantity: 1};
-            //console.log(index);
-            order.push(obj);
-        }else{
-            //console.log(index);
-            order[index].quantity++;
+        this.store.orderRestaurantSlug = localStorage.getItem('restaurantSlug');
+        // console.log(this.store.currentRestaurant.length);
+        if(this.store.currentRestaurant.length == undefined){
+            // console.log('get currentRestaurant');
+            // console.log(this.store.currentRestaurant);
+            let restaurant = localStorage.getItem('currentRestaurant');
+            this.store.currentRestaurant = JSON.parse(restaurant);
+            // console.log(localStorage);
+            // console.log(this.store.currentRestaurant);
+            // console.log(this.store.orderItems);
+            // console.log('end get currentRestaurant');
         }
-
-       });
-       console.log(order);
-
-       let arr = [];
-       order.forEach(food => {
-         for (let index = 0; index < food.quantity; index++) {
-            arr.push(food.id);
-         }
-       });
-       order = JSON.stringify(order);
-       console.log(JSON.parse(order));
 
     }
 }
 
-//mette l'ordine da store in localstorage
+//prende i dati da store e li salva in loscalStorage convertendoli in JSON
 function setLocalStorage() {
     //console.log('setLocalStorage');
     //trasforma l'array in store in una stringa json e la salva in localstorage
     let orderJson = JSON.stringify(this.store.orderItems);
     window.localStorage.setItem('order', orderJson);
+    let restaurantJson = JSON.stringify(this.store.currentRestaurant);
+    window.localStorage.setItem('currentRestaurant',restaurantJson);
+    // console.log('set currentREstaurant');
+    // console.log(localStorage);
+    //console.log(localStorage.currentRestaurant);
+    // console.log('set currentRestaurant');
 }
 
-//pulisce TUTTO il localstorage
+//svuota il localStorage e resetta i campi in Store
 function clearOrder() {
     this.store.orderRestaurantSlug = '',
     this.store.orderItems = [];
@@ -119,24 +149,27 @@ function clearOrder() {
 //prende da store l'array di id  orderItems e ritorna la quantità dell'id cercato
 function getQuantity(foodId) {
     //console.log('getQuantity');
-    var quantity = this.store.orderItems.filter(element => element == foodId);
-    return quantity.length;
-}
-
-function apiOrderRestaurant(){
-
-        this.axios.get('http://127.0.0.1:8000/api/restaurants/show', {
-            params: {
-
-                name: this.store.orderRestaurantSlug
-            }
-        })
-            .then(result => {
-                store.currentRestaurant = result.data.restaurant;
-        })
-
+    let quantity = 0;
+    let orderItems = this.store.orderItems;
+    let index = orderItems.findIndex(element => element['id'] == foodId);
+    if(index != -1){
+        quantity = orderItems[index].quantity;
+    }
+    return quantity;
 }
 
 
 
-export {getImagePath,getQuantity,clearOrder,setLocalStorage,getLocalStorage,removeFood,addFood,apiOrderRestaurant};
+function getFood(foodId){
+    // console.log('getfood');
+    let foods = this.store.currentRestaurant.foods;
+
+    let food = foods.find(element => element.id == foodId);
+    // console.log(food);
+    // console.log('end getFood');
+    return food
+}
+
+
+
+export {getImagePath,getQuantity,clearOrder,setLocalStorage,getLocalStorage,removeFood,addFood,getFood};
