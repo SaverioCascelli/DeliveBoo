4de0d2e9953e310c010fd2a925a4d67c0dbcdc60
RiccardoCivardi <riccardo.civardi@icloud.com>
2023-02-23 07:09:06 +0100

    fic cart show

--- resources/js/components/Cart.vue
+++ resources/js/components/Cart.vue
@@ -3,6 +3,7 @@
 <script>
 
     import { setLocalStorage, getLocalStorage, getQuantity, removeFood, addFood, clearOrder, getFood, foodTotalPrice, totalCartPrice } from '../data/function';
+    import { truncateText } from '../data/functionComputed';
     import { store } from '../data/store';
 
     export default {
@@ -22,27 +23,31 @@
             foodTotalPrice,
             totalCartPrice,
             //***fine funzioni chiamate da function.js */
-
-            //serve per le funzioni di aggiunta in localstorage
+            truncateText
 
         }
 
 
-    },
+        },
 
+        methods: {
 
+            getRestaurant() {
+                this.getLocalStorage();
 
-    methods: {
+            },
 
-        getRestaurant() {
-            this.getLocalStorage();
+            textTruncate(name, number = 20){
+
+                return truncateText(name, number);
+
+            }
 
         },
 
-    },
-    mounted() {
-        this.getRestaurant();
-    }
+        mounted() {
+            this.getRestaurant();
+        }
 
     }
 
@@ -51,37 +56,62 @@
 
 <template>
 
-    <div class="card p-2">
+    <div class="card p-2 mb-3">
 
 
+        <div v-for="(food, key) in store.orderItems" :key="key" class="mb-2">
 
-        <div v-for="(food, key) in store.orderItems">
-            <span>{{ getFood(food.id).name }}</span>
-            <button @click="removeFood(food.id)">remove</button>
-            <span>quantità : {{ getQuantity(food.id) }}</span>
-            <button @click="addFood(food.id)">add</button>
-
-
-
-            <div>
-                <p>price : {{ getFood(food.id).price }} per pezzo </p>
-                <p>totale articolo: {{ foodTotalPrice(food.id) }}</p>
+            <div class="d-flex align-items-center justify-content-between">
+                <p>{{ textTruncate(getFood(food.id).name) }}</p>
+                <small class="text-primary">&euro;{{ getFood(food.id).price }}</small>
             </div>
 
+            <div class="d-flex align-items-center justify-content-between">
 
+                <div class="buttons d-flex align-items-center justify-content-between mb-1">
 
+                    <div class="add-remove d-flex align-items-center justify-content-between">
 
-            <div>
-                <span>totale complessivo: </span>
-                <span>{{ totalCartPrice() }}</span>
+                        <button class="btn btn-outline-primary btn-sm" @click="removeFood(food.id)">
+                            <i class="fa-solid fa-minus"></i>
+                        </button>
+
+                        <span> {{ getQuantity(food.id) }}</span>
+
+                        <button class="btn btn-outline-primary btn-sm" @click="addFood(food.id)">
+                            <i class="fa-solid fa-plus"></i>
+                        </button>
+
+                    </div>
+
+                    <p class="mb-0">
+                        &euro;{{ foodTotalPrice(food.id) }}
+                    </p>
+
+                </div>
+
             </div>
+
         </div>
 
+            <div class="mt-2 text-end">
+                <span>TOTALE ORDINE:
+                    <strong>
+                        &euro;{{ totalCartPrice() }}
+                    </strong>
+                </span>
+            </div>
 
 
+            <div class="d-flex justify-content-end my-2">
 
+                <router-link :to="{name: 'cartPayment'}">
+                    <button class="btn btn-primary text-white">ORDINA</button>
+                </router-link>
 
-        <button class="btn btn-danger" @click="clearOrder()">SVUOTA CARRELLO</button>
+                <button class="ms-3 btn btn-danger" @click="clearOrder()">CANCELLA</button>
+
+            </div>
 
 
 
@@ -91,6 +121,24 @@
 
 
 <style lang="scss" scoped>
+
+    @use '../../scss/partialsVue/vars' as *;
+
+    .card {
+        background-color: $bg-light;
+        color: $black;
+        .buttons {
+            width: 90%;
+            margin: 0 auto;
+            padding: 5px 10px;
+            background-color: $success;
+            border-radius: 10px;
+            .add-remove {
+                width: 40%;
+            }
+
+        }
+    }
 
 
 </style>
